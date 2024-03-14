712688a4974c7e41fc2756449f4b444b3660c8fb
Robin Keskisärkkä <robin.keski@gmail.com>
2024-03-14 08:29:32 +0100

    publish modules and demo separately

--- build-dev.py
+++ build-dev.py
@@ -3,30 +3,42 @@ from glob import glob
 import os
 from rdflib import Graph
 import re
+import logging
+
+# Configure root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.basicConfig(format='%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 
 def copy_ontologies():
-    """Copy dev ontologies to web path."""
+    """Copy ontologies to web path."""
     map = {
         "modules": "ontology",
         "demo": "demo"
     }
-    
+
     for type in ["modules", "demo"]:
-        for source in glob(f"ontology/{type}/*/*/*", recursive=True):
+        # publish ontologies
+        for source in sorted(glob(f"ontology/{type}/*/*/*", recursive=True)):
             if not source.endswith(".ttl"):
                 continue
-
+            
             parts = re.match(f"ontology/{type}/([^/]*)/([^/]*)", source)    
             name = parts.group(1)
-            version = parts.group(2)
-            base = f"docs/{map[type]}/{name}/{version}/{name}"
-            os.makedirs(f"docs/{map[type]}/{name}/{version}/", exist_ok=True)
+            version = float(parts.group(2))
+            target = f"docs/{map[type]}/{name}/{version}/"
+            os.makedirs(target, exist_ok=True)
+            
+            logging.debug(f"Source:\t{source}")
+            logging.debug(f"Name:\t{name}")
+            logging.debug(f"Target:\t{target}")
+            
             g = Graph()
             g.parse(source)
-            g.serialize(destination=f"{base}.ttl", format="turtle")
-            g.serialize(destination=f"{base}.rdf", format="xml")
-            g.serialize(destination=f"{base}.owl", format="xml")
-            g.serialize(destination=f"{base}.jsonld", format="json-ld")
+            g.serialize(destination=f"{target}{name}.ttl", format="turtle")
+            g.serialize(destination=f"{target}{name}.rdf", format="xml")
+            g.serialize(destination=f"{target}{name}.owl", format="xml")
+            g.serialize(destination=f"{target}{name}.jsonld", format="json-ld")
 
 
 if __name__ == "__main__":
